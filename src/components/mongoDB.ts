03d54275849875999ab77c1f231f2b51208304e9
Xuân <xuanduc.xdt@gmail.com>
2021-12-30 16:02:38 +0700

    change database for app

--- src/components/mongoDB.ts
+++ src/components/mongoDB.ts
@@ -4,8 +4,8 @@ import config from '../config/index';
 
 class MongoDB {
   static connect(connectString?: string): any {
-    const { mongoDB: { host, port, testDatabase }, nodeEnv } = config;
-    const connectStr: string = connectString || `mongodb://${host}:${port}/${testDatabase}`;
+    const { mongoDB: { host, port, database }, nodeEnv } = config;
+    const connectStr: string = connectString || `mongodb://${host}:${port}/${database}`;
 
     mongoose.set('debug', nodeEnv === 'development');
     return mongoose.connect(connectStr);
